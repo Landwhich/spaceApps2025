dbd7a24d60a63643c6cec1469d2d437d218c42b5
Hibbuk <Shaelkotecha@outlook.com>
2025-10-05 16:49:57 -0400

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -4,26 +4,66 @@
 Servo servoX;
 Servo servoY;
 
+const int ldrLeft   = A0;
+const int ldrRight  = A1;
+const int ldrTop    = A2;
+const int ldrBottom = A3;
+
 int posX = 0;
 int posY = 0;
+
 const int targetRange = 120;
 const int yTargetRange = 120;
 const int xTargetRange = 120;
-
 const int yTimeout = 20;
 const int xTimeout = 100;
 
 const int yIncrement = 2;
 const int xIncrement = 3;
 
+const int threshold = 50;
 // apag aphemerus
 
 void setup() {
   servoX.attach(9);
   servoY.attach(10);
+
+  servoX.write(posX);
+  servoY.write(posY);
+
+  Serial.begin(9600);
 }
 
 void loop() {
+   // Read LDRs
+  int leftVal   = analogRead(ldrLeft);
+  int rightVal  = analogRead(ldrRight);
+  int topVal    = analogRead(ldrTop);
+  int bottomVal = analogRead(ldrBottom);
+
+  // Differences
+  int diffX = leftVal - rightVal;
+  int diffY = topVal - bottomVal;
+
+  // LDR tracking
+  if (diffX > threshold && posX < 180) posX++;       // move left
+  else if (diffX < -threshold && posX > 0) posX--;  // move right
+
+  if (diffY > threshold && posY < 180) posY++;       // move up
+  else if (diffY < -threshold && posY > 0) posY--;  // move down
+
+  // Write positions
+  servoX.write(posX);
+  servoY.write(posY);
+
+  // Debug
+  Serial.print("LDR Left: "); Serial.print(leftVal);
+  Serial.print(" | Right: "); Serial.print(rightVal);
+  Serial.print(" | Top: "); Serial.print(topVal);
+  Serial.print(" | Bottom: "); Serial.print(bottomVal);
+  Serial.print(" | ServoX: "); Serial.print(posX);
+  Serial.print(" | ServoY: "); Serial.println(posY);
+  
   servoSweep();
 }
 
